9b7488080df4fdd66ff07798b38d0c71f3fd4497
saam2801 <samugsilva2017@gmail.com>
2026-03-20 17:42:08 -0300

    Básico do app finalizado

--- MinhasCompras/Helpers/SQLiteDatabaseHelper.cs
+++ MinhasCompras/Helpers/SQLiteDatabaseHelper.cs
@@ -22,7 +22,7 @@ namespace MinhasCompras.Helpers
         }
         public Task<List<Produto>> Update(Produto p)
         {
-            string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=?, WHERE Id=?";
+            string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=? WHERE Id=?";
             return _conn.QueryAsync<Produto>(sql, p.Descricao, p.Quantidade, p.Preco, p.Id);
         }
         public Task<int> Delete(int id)
